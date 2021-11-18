e3caa41a256fdd9665b3477bc7471874e61878e4
xuangiangit1 <ngxuangiang2020@gmail.com>
2021-11-18 22:09:16 +0700

    Update module profile and change password function

---
+++ resources/views/backend/user/edit_password.blade.php
@@ -0,0 +1,95 @@
+@extends('admin.admin_master')
+@section('admin')
+
+
+    <div class="content-wrapper">
+        <div class="container-full">
+            <!-- Content Header (Page header) -->
+
+
+            <section class="content" style="margin: 0 5%">
+
+                <!-- Basic Forms -->
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h4 class="box-title">Thay đổi mật khẩu</h4>
+
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col">
+
+                                <form method="post" action="{{ route('password.update') }}">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-12">
+
+                                            <div class="form-group">
+                                                <h5>Nhập mật khẩu hiện tại <span class="text-danger">*</span></h5>
+                                                <div class="controls">
+                                                    <input type="password" name="oldpassword" id="current_password" class="form-control" >
+                                                    @error('oldpassword')
+                                                    <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+
+                                            </div>
+
+
+
+                                            <div class="form-group">
+                                                <h5>Nhập mật khẩu mới <span class="text-danger">*</span></h5>
+                                                <div class="controls">
+                                                    <input type="password" name="password" id="password" class="form-control"  >
+                                                    @error('password')
+                                                    <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+
+                                            </div>
+
+                                            <div class="form-group">
+                                                <h5>Nhập lại mật khẩu mới  <span class="text-danger">*</span></h5>
+                                                <div class="controls">
+                                                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" >
+                                                    @error('password_confirmation')
+                                                    <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+
+                                                </div>
+
+                                            </div>
+
+
+
+                                            <div class="text-xs-right">
+                                                <input type="submit" class="btn btn-rounded btn-info mb-5" value="Submit">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+
+            </section>
+
+
+
+
+
+        </div>
+    </div>
+
+
+
+
+
+@endsection
---
+++ resources/views/backend/user/view_profile.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.admin_master')
+@section('admin')
+
+
+    <div class="content-wrapper">
+        <div class="container-full">
+            <section class="content">
+                <div class="row">
+
+
+                    <div class="col-12">
+
+
+                        <div class="box box-widget widget-user">
+                            <!-- Add the bg color to the header using any of the bg-* classes -->
+
+                            <div class="widget-user-header bg-black" >
+                                <h3 class="widget-user-username"><b>{{ $user->name }}</b> <h6>{{ $user->gender }}</h6></h3>
+                                <a href="{{ route('profile.edit') }}" style="float: right;"
+                                   class="btn btn-rounded btn-success mb-5"> Chỉnh sửa</a>
+
+
+                            </div>
+                            <div class="widget-user-image" >
+                                <img class="rounded-circle"
+                                     src="{{ (!empty($user->image))? url('upload/user_images/'.$user->image):url('upload/no_image.jpg') }} "
+                                     alt="User Avatar">
+                            </div>
+                            <div class="box-footer">
+                                <div class="row" style="margin-top: 3%">
+                                    <div class="col-sm-4">
+                                        <div class="description-block">
+                                            <h5 class="description-header">Điện thoại</h5>
+                                            <span class="description-text">{{ $user->mobile }}</span>
+                                        </div>
+                                        <!-- /.description-block -->
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="description-block">
+                                            <h5 class="description-header">Email</h5>
+                                            <span class="description-text">{{ $user->email }}</span>
+{{--                                            <h6 class="widget-user-desc"><b>{{ $user->email }}</b></h6>--}}
+                                        </div>
+                                        <!-- /.description-block -->
+                                    </div>
+
+                                    <!-- /.col -->
+                                    <div class="col-sm-4 br-1 bl-1">
+                                        <div class="description-block">
+                                            <h5 class="description-header">Địa chỉ</h5>
+                                            <span class="description-text">{{ $user->address }}</span>
+                                        </div>
+                                        <!-- /.description-block -->
+                                    </div>
+                                    <!-- /.col -->
+{{--                                    <div class="col-sm-4">--}}
+{{--                                        <div class="description-block">--}}
+{{--                                            <h5 class="description-header">Giới tính</h5>--}}
+{{--                                            <span class="description-text">{{ $user->gender }}</span>--}}
+{{--                                        </div>--}}
+{{--                                        <!-- /.description-block -->--}}
+{{--                                    </div>--}}
+                                    <!-- /.col -->
+
+                                </div>
+                                <!-- /.row -->
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </section>
+        </div>
+    </div>
+@endsection
