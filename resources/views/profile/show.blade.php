4dcf2e33f746efb9592b7a0c5dcd6a1bc5565d74
Xuan Giang <ngxuangiang2020@gmail.com>
2022-01-04 09:42:41 +0700

    fix page setup account

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,11 +1,9 @@
+@extends('admin.admin_master')
+@section('admin')
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
 
-    <div>
+
+    <div style="margin-left: 18%">
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
 {{--            @if (Laravel\Fortify\Features::canUpdateProfileInformation())--}}
 {{--                @livewire('profile.update-profile-information-form')--}}
@@ -43,3 +41,4 @@
         </div>
     </div>
 </x-app-layout>
+@endsection
